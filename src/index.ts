42e09645bf78b25495185c4a359c4fd7bc544b47
Wise Introvert <fardeen.wi.panjwani@gmail.com>
2023-08-13 12:21:38 +0530

    refactor: firebase, routes

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import * as bodyParser from 'body-parser'
 import * as cookieParser from 'cookie-parser'
 import * as express from 'express'
 import * as logger from 'morgan'
-import * as path from 'path'
-import { Response } from 'express'
+
+import routes from './routes'
+import admin from './config/admin'
 
 const app: express.Express = express()
 
@@ -11,13 +12,33 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, '../../src/public')))
+app.use('/api', routes)
 
-app.get('/', (_, res: Response<{ message: string }>) => {
+app.get('/api/health', async (_, res: express.Response): Promise<void> => {
+  try {
+    await admin.firestore().collection('health').get()
+    res
+      .status(200)
+      .json({
+        status: 'healthy',
+      })
+      .end()
+  } catch (err) {
+    res
+      .status(500)
+      .json({
+        error: err.message,
+        status: 'sick',
+      })
+      .end()
+  }
+})
+
+app.get('/*', (_, res: express.Response<{ error: string }>) => {
   res
-    .status(200)
+    .status(404)
     .json({
-      message: 'hello',
+      error: 'Not Found',
     })
     .end()
 })
